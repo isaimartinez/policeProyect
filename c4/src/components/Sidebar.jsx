67695fef9d8b75f9d2dbc599dd7972df9d511dcb
Isai Martinez <isai.martinez@yeico.com>
2022-12-22 16:40:23 -0600

    removing trash

--- c4/src/components/Sidebar.jsx
+++ c4/src/components/Sidebar.jsx
@@ -24,50 +24,7 @@ const Sidebar = () => {
         activeMenu && ( <>
           <div className='flex flex-col w-72 justify-between mt-20 mb-10'>
             <div className='flex flex-col items-center'>
-                <Filters />
-              {/* <LocalizationProvider  dateAdapter={AdapterDayjs}>
-                <DesktopDatePicker
-                  className='bg-white flex w-[250px] '
-                  style={{width: 250}}
-                  inputFormat="MM/DD/YYYY"
-                  value={date}
-                  onChange={newDate =>dispatch(setDate(newDate))}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              </LocalizationProvider>
-              <FormControl className='bg-white' sx={{ m: 1, width: 250 }}>
-                <InputLabel id="chipLabel">Zonas</InputLabel>
-                <Select
-                  labelId="chipLabel"
-                  id="demo-multiple-chip"
-                  multiple
-                  value={selectedZones}
-                  onChange={handleChange}
-                  input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                  renderValue={(selected) => (
-                    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                      {selected.map((value) => (
-                        <Chip key={value} label={value} />
-                      ))}
-                    </Box>
-                  )}
-                  MenuProps={MenuProps}
-                >
-                  {zones.map(({name}) => (
-                    <MenuItem
-                      key={name}
-                      value={name}
-                      style={getStyles(name, selectedZones, theme)}
-                    >
-                      {name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <FormControl className='bg-white' sx={{ m: 1, width: 250 }}>
-                <FormControlLabel className='bg-white mx-1' control={<Checkbox checked={showZones} onChange	={(e) => {dispatch(setShowZones(!showZones))}} />} label="Mostrar Zonas" />
-                <FormControlLabel className='bg-white' control={<Checkbox checked={showTraffic} onChange	={(e) => {dispatch(setShowTraffic(!showTraffic))}} />} label="Mostrar Tráfico" />
-              </FormControl> */}
+              <Filters />
               <NewZone />
             </div>
             <div className='flex flex-row justify-center items-center cursor-pointer gap-2 hover:bg-slate-100 rounded m-2 p-1'
