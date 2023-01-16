d5f73007c97e683d2b72c0d1e79d4af69296c475
Isai Martinez <isai.martinez@yeico.com>
2023-01-16 13:12:58 -0600

    disabled btn when inputs = ""

--- c4/src/pages/Login/hooks.js
+++ c4/src/pages/Login/hooks.js
@@ -4,8 +4,24 @@ import {fetchLogin} from '../../redux/reducers/authSlice'
 
 
 export const useLogin = () => {
-  const [id, setId] = useState("848b1a03-d950-4978-82ae-3a3dcfedf943")
-  const [key, setKey] = useState("71e9862a-c1de-455a-8f1d-71d5f998e6a8")
+  // const [id, setId] = useState("848b1a03-d950-4978-82ae-3a3dcfedf943")
+  // const [key, setKey] = useState("71e9862a-c1de-455a-8f1d-71d5f998e6a8")
+
+  const [id, setId] = useState("")
+  const [key, setKey] = useState("")
+  const [isDisabledBtn, setIsDisabledBtn] = useState(false)
+
+  useEffect(() => {
+    if(id.length == 0 || key.length == 0){
+      setIsDisabledBtn(true)
+    } else{
+      setIsDisabledBtn(false)
+    }
+  }, [id, key])
+  
+
+  // const [id, setId] = useState("")
+  // const [key, setKey] = useState("")
   const dispatch = useDispatch()
 
 
@@ -14,5 +30,5 @@ export const useLogin = () => {
     dispatch(fetchLogin({id, key}))
   }
 
-  return {id, setId, key, setKey, handleLogin}
+  return {id, setId, key, setKey, handleLogin,  isDisabledBtn}
 }
