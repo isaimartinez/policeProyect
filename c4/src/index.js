80c2af9edf8cbab4c114051da0aafce3e3214945
Isai Martinez <isai.martinez@yeico.com>
2023-01-16 11:58:39 -0600

    Absolute path

--- c4/src/index.js
+++ c4/src/index.js
@@ -7,9 +7,8 @@ import {BrowserRouter} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import 'react-h5-audio-player/lib/styles.css';
-
-import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import {store} from 'redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
