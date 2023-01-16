b53c3d2bbb3995253ee4e50e3c5e7b314b3045c9
Isai Martinez <isai.martinez@yeico.com>
2023-01-16 13:13:31 -0600

    Login tests

--- c4/src/pages/Login/Login.jsx
+++ c4/src/pages/Login/Login.jsx
@@ -4,15 +4,15 @@ import Button from '@mui/material/Button';
 import { useLogin } from './hooks';
 
 const Login = () => {
-  const {id, setId, key, setKey, handleLogin} = useLogin()
+  const {id, setId, key, setKey, handleLogin, isDisabledBtn} = useLogin()
 
   return (
     <div className='flex w-full h-screen m-auto justify-center items-center'>
       <form className='flex flex-col gap-5 w-96' onSubmit={handleLogin}>
-        <TextField id="filled-basic" label="Id" variant="filled" value={id} onChange={e => setId(e.target.value)}/>
-        <TextField id="filled-basic" label="Password" variant="filled" value={key} onChange={e => setKey(e.target.value)}/>
+        <TextField id="filled-basic" type={"text"} label="Id" variant="filled" value={id} onChange={e => setId(e.target.value)}/>
+        <TextField id="filled-basic" type={"password"} label="Password" variant="filled" value={key} onChange={e => setKey(e.target.value)}/>
 
-        <Button type='submit' variant="outlined" size='large'>Iniciar Sesión</Button>
+        <Button disabled={isDisabledBtn} type='submit' variant="outlined" size='large'>Iniciar Sesión</Button>
       </form>
     </div>
   )
--- c4/src/pages/__tests__/Login.test.js
+++ c4/src/pages/__tests__/Login.test.js
@@ -15,17 +15,69 @@ const render = component => rtlRender(
 
 test('Id input should be rendered', () => {
   render(<Login />)
-
-  const idInput = screen.getByText(/Id/i)
-
+  const idInput = screen.getByLabelText(/Id/i)
   expect(idInput).toBeInTheDocument()
-
 })
 
 test('Password input should be rendered', () => {
   render(<Login />)
-
-  const keyInput = screen.getByText(/Password/i)
+  const keyInput = screen.getByLabelText(/Password/i)
   expect(keyInput).toBeInTheDocument()
+})
+
+test('On both empty inputs disabled login button', () => {
+  render(<Login />)
+  const LoginBtn = screen.getByText(/Iniciar Sesión/i)
+  expect(LoginBtn).toBeDisabled()
+})
+
+test('On empty ID input disabled login button', () => {
+  render(<Login />)
+  const LoginBtn = screen.getByText(/Iniciar Sesión/i)
+  const idInput = screen.getByLabelText(/Id/i)
+  fireEvent.change(idInput.closest("input"), {target: {value: 'Lorem'}})
+  expect(LoginBtn).toBeDisabled()
+})
+
+test('On empty Password input disabled login button', () => {
+  render(<Login />)
+  const LoginBtn = screen.getByText(/Iniciar Sesión/i)
+  const pswInput = screen.getByLabelText(/Password/i)
+  fireEvent.change(pswInput.closest("input"), {target: {value: 'Lorem'}})
+  expect(LoginBtn).toBeDisabled()
+})
+test("Id input should change", () => {
+  render(<Login />);
+  const pswInput = screen.getByLabelText(/Password/i);
+  const testValue = "test";
+  fireEvent.change(pswInput, { target: { value: testValue } });
+  expect(pswInput.value).toBe(testValue);
+});
+
+test("Password input should change", () => {
+  render(<Login />);
+  const idInput = screen.getByLabelText(/Id/i);
+  const testValue = "test";
+
+  fireEvent.change(idInput, { target: { value: testValue } });
+  expect(idInput.value).toBe(testValue);
+});
+
+test('Enabled login button when id and password inputs have text', () => {
+  render(<Login />)
+  const pswInput = screen.getByLabelText(/Password/i)
+  const idInput = screen.getByLabelText(/Id/i)
+  const testValue = "test";
+
+  fireEvent.change(pswInput, {target: {value: testValue}})
+  fireEvent.change(idInput, {target: {value: testValue}})
+  expect(idInput.value).toBe(testValue);
+  expect(pswInput.value).toBe(testValue);
+  const LoginBtn = screen.getByText(/Iniciar Sesión/i)
+  console.log("hey",LoginBtn)
+  expect(LoginBtn.closest('button')).not.toBeDisabled();
 
 })
+
+
+
